8586bf357c0fb8f5fbc1423baa4d978304b1031e
Yannis Sgarra <hello@yannissgarra.com>
2023-08-26 19:33:18 +0200

    feat: get translator catalogue

--- src/Translation/TranslatorAwareTrait.php
+++ src/Translation/TranslatorAwareTrait.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace Webmunkeez\I18nBundle\Translation;
 
+use Symfony\Component\Translation\MessageCatalogueInterface;
+use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -28,5 +30,17 @@ trait TranslatorAwareTrait
     protected function trans(string $id, array $parameters = [], ?string $domain = null, ?string $locale = null): string
     {
         return $this->translator->trans($id, $parameters, $domain, $locale);
+    }
+
+    /**
+     * @throws \LogicException
+     */
+    protected function getTranslatorCatalogue(?string $locale = null): MessageCatalogueInterface
+    {
+        if ($this->translator instanceof TranslatorBagInterface) {
+            return $this->translator->getCatalogue($locale);
+        }
+
+        throw new \LogicException('Translator must implements '.MessageCatalogueInterface::class.' for using `getTranslatorCatalogue` method.');
     }
 }
